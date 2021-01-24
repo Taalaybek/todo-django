1b51e57afd6436c1b4e88ea38e2b10322e2810c7
Alex Bowey <alex.bowey@yahoo.com>
2021-01-25 04:06:06 +0600

    added static for urls

--- todo/urls.py
+++ todo/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from main.views import homepage
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", homepage, name="home")
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_URL) \
+  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
