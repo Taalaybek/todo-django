1e8b4971f3dfb5d2d34f6d9127530177dc2a2a2f
Alex Bowey <alex.bowey@yahoo.com>
2021-01-25 12:50:54 +0600

    wrote view function for display books

--- main/views.py
+++ main/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, HttpResponse
-from .models import ToDo
+from .models import ToDo, Book
 
 # Create your views here.
 def homepage(request):
   todos = ToDo.objects.all()
   return render(request, "index.html", {"todos": todos})
+
+def books(request):
+  books = Book.objects.all()
+  return render(request, "books.html", {"books": books})
