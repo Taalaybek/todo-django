90d44246c57c9cc3ea0cd4ed4e59b42c23763307
Alex Bowey <alex.bowey@yahoo.com>
2021-01-25 09:33:50 +0600

    created ToDo model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class ToDo(models.Model):
+  title = models.CharField(max_length=100)
+  created_at = models.DateField(auto_now_add=True)
+  is_favorite = models.BooleanField(default=False)
+  is_done = models.BooleanField(default=False)
