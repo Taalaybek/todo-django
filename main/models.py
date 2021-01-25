6aaf3b9000d2dbe559a45db0bd05dfd1a828a067
Alex Bowey <alex.bowey@yahoo.com>
2021-01-25 15:45:27 +0600

    added description and price, updated todo

--- main/models.py
+++ main/models.py
@@ -2,15 +2,15 @@ from django.db import models
 
 class ToDo(models.Model):
   title = models.CharField(max_length=100)
-  created_at = models.DateField(auto_now_add=True)
+  created_at = models.DateTimeField(auto_now_add=True)
   is_favorite = models.BooleanField(default=False)
   is_done = models.BooleanField(default=False)
 
 class Book(models.Model):
   title = models.CharField(max_length=255)
   subtitle = models.CharField(max_length=200)
-  description = models.TextField
-  price = models.IntegerField
+  description = models.TextField(null=True)
+  price = models.IntegerField(null=True)
   genre = models.CharField(max_length=150)
   author = models.CharField(max_length=255)
   year = models.DateField(auto_now_add=False)
