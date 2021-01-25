bb5ef1c682876825ee1d21d37512f672273c5c8a
Alex Bowey <alex.bowey@yahoo.com>
2021-01-25 12:51:11 +0600

    created books route

--- todo/urls.py
+++ todo/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main.views import homepage
+from main.views import homepage, books
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", homepage, name="home"),
+    path("books", books, name="books"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_URL) \
   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
