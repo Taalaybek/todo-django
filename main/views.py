36b73c4837ac7d56d7f20ebf2150178912fe7165
Alex Bowey <alex.bowey@yahoo.com>
2021-01-25 15:46:42 +0600

    handle creating book

--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .models import ToDo, Book
 
 # Create your views here.
@@ -9,3 +9,19 @@ def homepage(request):
 def books(request):
   books = Book.objects.all()
   return render(request, "books.html", {"books": books})
+
+def add_todo(request):
+  todo = ToDo(title=request.POST['title'])
+  todo.save()
+  return redirect(homepage)
+
+def add_book(request):
+  title = request.POST['title']
+  subtitle = request.POST['subtitle']
+  date = request.POST['date']
+  author = request.POST['author']
+  genre = request.POST['genre']
+  description = request.POST['description']
+  price = request.POST['price']
+  Book(title=title, subtitle=subtitle, year=date, genre=genre, author=author, description=description, price=price).save()
+  return redirect(books)
