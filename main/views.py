3a43e6fa7186462419c8c313688a943f8e4b1e6d
Alex Bowey <alex.bowey@yahoo.com>
2021-01-25 11:09:27 +0600

    passed todos from db

--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponse
+from .models import ToDo
 
 # Create your views here.
 def homepage(request):
-  return render(request, "index.html")
+  todos = ToDo.objects.all()
+  return render(request, "index.html", {"todos": todos})
