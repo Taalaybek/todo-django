1c423e42d1015853e1b7666d0088fe5ce60c467d
Alex Bowey <alex.bowey@yahoo.com>
2021-01-25 12:50:34 +0600

    created new model - Book

--- main/models.py
+++ main/models.py
@@ -5,3 +5,13 @@ class ToDo(models.Model):
   created_at = models.DateField(auto_now_add=True)
   is_favorite = models.BooleanField(default=False)
   is_done = models.BooleanField(default=False)
+
+class Book(models.Model):
+  title = models.CharField(max_length=255)
+  subtitle = models.CharField(max_length=200)
+  description = models.TextField
+  price = models.IntegerField
+  genre = models.CharField(max_length=150)
+  author = models.CharField(max_length=255)
+  year = models.DateField(auto_now_add=False)
+  date = models.DateField(auto_now_add=True)
